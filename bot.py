393828635e26cbf74d6e4517e0a4c1b16fb42565
Rogerxt9k <81235122+Rogerxt9k@users.noreply.github.com>
2023-12-06 18:38:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/Bihari_Anime")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/primeprosmovies")
         self.LOGGER(__name__).info(f""" \n\n       
 ░█████╗░░█████╗░██████╗░███████╗██╗░░██╗██████╗░░█████╗░████████╗███████╗
 ██╔══██╗██╔══██╗██╔══██╗██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗╚══██╔══╝╚════██║
