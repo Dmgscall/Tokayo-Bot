650061e79b6faa7ea9696e1a2b5c48f0762f3295
Rogerxt9k <81235122+Rogerxt9k@users.noreply.github.com>
2023-12-08 19:56:53 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -124,7 +124,7 @@ async def not_joined(client: Client, message: Message):
         [
             InlineKeyboardButton(
                 "Join Channel",
-                url = https://t.me/linkclickseason2)
+                url = 'https://t.me/linkclickseason2')
         ]
     ]
     try:
